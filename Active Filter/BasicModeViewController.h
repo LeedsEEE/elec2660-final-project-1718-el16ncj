5e339700787585da50f7f4e391f94c34ad22ab76
Noel Julian <BackSlash251@tejass-mac-mini.local>
2017-12-02 16:58:39 +0000

    Added a second view controller for the Basic Mode. Added the required labels and a back button to go back to the main menu.

---
+++ Active Filter/BasicModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BasicModeViewController.h
+//  Active Filter
+//
+//  Created by Noel Julian on 02/12/2017.
+//  Copyright © 2017 Noel Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicModeViewController : UIViewController
+
+@end
