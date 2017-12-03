71d63a4c457aa597c0dd32778b459e8035a9dd55
Noel Julian <BackSlash251@tejass-mac-mini.local>
2017-12-03 11:57:44 +0000

    Added DataModel class to carry out the calculations.

---
+++ Active Filter/DataModel.h
@@ -0,0 +1,13 @@
+//
+//  DataModel.h
+//  Active Filter
+//
+//  Created by Noel Julian on 03/12/2017.
+//  Copyright © 2017 Noel Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+
+@end
---
+++ Active Filter/DataModel.m
@@ -0,0 +1,13 @@
+//
+//  DataModel.m
+//  Active Filter
+//
+//  Created by Noel Julian on 03/12/2017.
+//  Copyright © 2017 Noel Julian. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+
+@end
